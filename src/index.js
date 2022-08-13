20c8a88e2804dcfad5d86205aa3200bdf581477c
unknown <ajfoucaultmo@mail.usf.edu>
2022-08-12 22:48:11 -0700

    16. First Component

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+function Greeting() {
+  return <h4>this is Alejandro and this is my first component</h4>;
+}
+
+ReactDOM.render(<Greeting />, document.getElementById('root'));
