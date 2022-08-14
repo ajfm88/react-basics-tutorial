9d4b54bd1c9a15798c8226e0742246ce876a1667
unknown <ajfoucaultmo@mail.usf.edu>
2022-08-14 15:56:49 -0700

    19. JSX Rules

--- src/index.js
+++ src/index.js
@@ -1,23 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-// stateless functional component
-// always return JSX
+// JSX Rules
+// return single element
+// div /  selection / article or Fragment
+// use camelCase for html attribute
+// className instead of class
+// close every element
+// formatting
 
 function Greeting() {
   return (
-    <div>
-      <h1>Hello World!</h1>
-    </div>
+    <section>
+      <article>
+        <h3>Hello people</h3>
+        <ul>
+          <li>
+            <a href='#'>hello world</a>
+          </li>
+        </ul>
+        <h1>Hello World!</h1>
+      </article>
+    </section>
   );
 }
-
-// const Greeting = () => {
-//   return React.createElement(
-//     'div',
-//     {},
-//     React.createElement('h1', {}, 'hello world')
-//   );
-// };
 
 ReactDOM.render(<Greeting />, document.getElementById('root'));
