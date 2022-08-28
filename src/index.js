be1fa7f126eece38acec97cf3273ed180385cc1d
unknown <ajfoucaultmo@mail.usf.edu>
2022-08-28 10:26:17 -0700

    25. Props

--- src/index.js
+++ src/index.js
@@ -3,26 +3,40 @@ import ReactDOM from 'react-dom';
 
 // CSS
 import './index.css';
+// setup vars
+const firstBook = {
+  img: 'https://m.media-amazon.com/images/I/91hIsjFuICL._AC_UL320_.jpg',
+  title: 'I Love You to the Moon and Back',
+  author: 'Amelia Hepworth',
+};
+const secondBook = {
+  img: 'https://m.media-amazon.com/images/I/71aLultW5EL._AC_UY218_.jpg',
+  title: 'Our Class is a Family',
+  author: 'Shannon Olsen',
+};
+
 function BookList() {
   return (
     <section className='booklist'>
-      <Book />
+      <Book
+        img={firstBook.img}
+        title={firstBook.title}
+        author={firstBook.author}
+      />
+      <Book
+        img={secondBook.img}
+        title={secondBook.title}
+        author={secondBook.author}
+      />
     </section>
   );
 }
-const author = 'Amelia Hepworth';
-const Book = () => {
-  const title = 'I Love You to the Moon and Back';
+const Book = (props) => {
   return (
     <article className='book'>
-      <img
-        src='https://m.media-amazon.com/images/I/91hIsjFuICL._AC_UL320_.jpg'
-        alt=''
-      />
-      <h1>{title}</h1>
-      <h4>{author.toUpperCase()}</h4>
-      {/* <p>{let x = 6}</p> */}
-      <p>{6 + 6}</p>
+      <img src={props.img} alt='' />
+      <h1>{props.title}</h1>
+      <h4>{props.author}</h4>
     </article>
   );
 };
