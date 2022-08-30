edc7f9cf9142535bcf61be6df4143dff257b0e22
unknown <ajfoucaultmo@mail.usf.edu>
2022-08-29 19:39:30 -0700

    30. Key Prop And Spread Operator

--- src/index.js
+++ src/index.js
@@ -6,16 +6,19 @@ import './index.css';
 // setup vars
 const books = [
   {
+    id: 1,
     img: 'https://m.media-amazon.com/images/I/91hIsjFuICL._AC_UL320_.jpg',
     title: 'I Love You to the Moon and Back',
     author: 'Amelia Hepworth',
   },
   {
+    id: 2,
     img: 'https://m.media-amazon.com/images/I/71aLultW5EL._AC_UY218_.jpg',
     title: 'Our Class is a Family',
     author: 'Shannon Olsen',
   },
   {
+    id: 3,
     img: 'https://m.media-amazon.com/images/I/81ICvbFe2+L._AC_UY218_.jpg',
     title: 'The Vanishing Half: A Novel',
     author: 'Brit Bennett',
@@ -25,15 +28,15 @@ const books = [
 function BookList() {
   return (
     <section className='booklist'>
-      {books.map((book) => {
-        return <Book book={book}></Book>;
+      {books.map((book, index) => {
+        return <Book key={book.id} {...book}></Book>;
       })}
     </section>
   );
 }
 
-const Book = (props) => {
-  const { img, title, author } = props.book;
+const Book = ({ img, title, author }) => {
+  // const { img, title, author } = props;
 
   return (
     <article className='book'>
