1eedad4835b965a6e5aab856714e37d244012e1d
unknown <ajfoucaultmo@mail.usf.edu>
2022-08-29 19:13:24 -0700

    28. Simple List

--- src/index.js
+++ src/index.js
@@ -4,48 +4,35 @@ import ReactDOM from 'react-dom';
 // CSS
 import './index.css';
 // setup vars
-const firstBook = {
-  img: 'https://m.media-amazon.com/images/I/91hIsjFuICL._AC_UL320_.jpg',
-  title: 'I Love You to the Moon and Back',
-  author: 'Amelia Hepworth',
-};
-const secondBook = {
-  img: 'https://m.media-amazon.com/images/I/71aLultW5EL._AC_UY218_.jpg',
-  title: 'Our Class is a Family',
-  author: 'Shannon Olsen',
-};
-
+const books = [
+  {
+    img: 'https://m.media-amazon.com/images/I/91hIsjFuICL._AC_UL320_.jpg',
+    title: 'I Love You to the Moon and Back',
+    author: 'Amelia Hepworth',
+  },
+  {
+    img: 'https://m.media-amazon.com/images/I/71aLultW5EL._AC_UY218_.jpg',
+    title: 'Our Class is a Family',
+    author: 'Shannon Olsen',
+  },
+];
+const names = ['john', 'peter', 'susan'];
+const newNames = names.map((name) => {
+  return <h1>{name}</h1>;
+});
+console.log(newNames);
 function BookList() {
-  return (
-    <section className='booklist'>
-      <Book
-        img={firstBook.img}
-        title={firstBook.title}
-        author={firstBook.author}
-      >
-        <p>
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Blanditiis
-          excepturi dignissimos dolore consectetur saepe ipsa ex fugiat quos,
-          earum nam.
-        </p>
-      </Book>
-      <Book
-        img={secondBook.img}
-        title={secondBook.title}
-        author={secondBook.author}
-      />
-    </section>
-  );
+  return <section className='booklist'>{newNames}</section>;
 }
+
 const Book = (props) => {
-  const { img, title, author, children } = props;
+  const { img, title, author } = props;
 
   return (
     <article className='book'>
       <img src={img} alt='' />
       <h1>{title}</h1>
       <h4>{author}</h4>
-      {children}
     </article>
   );
 };
