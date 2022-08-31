ea20f66d7750d253335350a005b3e5c7022f326e
unknown <ajfoucaultmo@mail.usf.edu>
2022-08-30 22:29:58 -0700

    32. Import And Export Statements

--- src/index.js
+++ src/index.js
@@ -1,69 +1,22 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDom from 'react-dom';
 
 // CSS
 import './index.css';
-// setup vars
-const books = [
-  {
-    id: 1,
-    img: 'https://m.media-amazon.com/images/I/91hIsjFuICL._AC_UL320_.jpg',
-    title: 'I Love You to the Moon and Back',
-    author: 'Amelia Hepworth',
-  },
-  {
-    id: 2,
-    img: 'https://m.media-amazon.com/images/I/71aLultW5EL._AC_UY218_.jpg',
-    title: 'Our Class is a Family',
-    author: 'Shannon Olsen',
-  },
-  {
-    id: 3,
-    img: 'https://m.media-amazon.com/images/I/81ICvbFe2+L._AC_UY218_.jpg',
-    title: 'The Vanishing Half: A Novel',
-    author: 'Brit Bennett',
-  },
-];
+
+import { data } from './books';
+import SpecificBook from './Book';
+import { greeting } from './testing/testing';
 
 function BookList() {
+  console.log(greeting);
   return (
     <section className='booklist'>
-      {books.map((book, index) => {
-        return <Book key={book.id} {...book}></Book>;
+      {data.map((book, index) => {
+        return <SpecificBook key={book.id} {...book}></SpecificBook>;
       })}
     </section>
   );
 }
 
-const Book = ({ img, title, author }) => {
-  // attribute, eventHandler
-  // onClick, onMouseOver
-  const clickHandler = (e) => {
-    console.log(e);
-    console.log(e.target);
-    alert('hello world');
-  };
-  const compleExample = (author) => {
-    console.log(author);
-  };
-  return (
-    <article
-      className='book'
-      onMouseOver={() => {
-        console.log(title);
-      }}
-    >
-      <img src={img} alt='' />
-      <h1 onClick={() => console.log(title)}>{title}</h1>
-      <h4>{author}</h4>
-      <button type='button' onClick={clickHandler}>
-        reference example
-      </button>
-      <button type='button' onClick={() => compleExample(author)}>
-        more complex example
-      </button>
-    </article>
-  );
-};
-
-ReactDOM.render(<BookList />, document.getElementById('root'));
+ReactDom.render(<BookList />, document.getElementById('root'));
